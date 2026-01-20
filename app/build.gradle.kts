1ba13bc922c5c65d94f33ca2410235369f7cfd5f
Krishna Vemra <krishnadjb9@gmail.com>
2026-01-21 01:11:35 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,5 +53,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation("androidx.browser:browser:1.8.0")
+    implementation "androidx.browser:browser:1.8.0"
+
 
 }
